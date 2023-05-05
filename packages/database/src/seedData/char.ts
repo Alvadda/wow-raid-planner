342457f43d9313458d0515ad29ccd2f8f04337f6
Christian Pansch <christian.pansch@uxma.com>
2023-05-05 18:43:14 +0200

    init turbo with prisma

---
+++ packages/database/src/seedData/char.ts
@@ -0,0 +1,176 @@
+export const charData = [
+  {
+    class: "Death Knight",
+    specs: [
+      {
+        name: "Blood",
+        role: "Tank",
+      },
+      {
+        name: "Frost",
+        role: "Mdps",
+      },
+      {
+        name: "Unholy",
+        role: "Mdps",
+      },
+    ],
+  },
+  {
+    class: "Druid",
+    specs: [
+      {
+        name: "Balance",
+        role: "Rdps",
+      },
+      {
+        name: "Feral Combat",
+        role: "Mdps",
+      },
+      {
+        name: "Restoration",
+        role: "Healer",
+      },
+      {
+        name: "Guardian",
+        role: "Tank",
+      },
+    ],
+  },
+  {
+    class: "Hunter",
+    specs: [
+      {
+        name: "Beast Mastery",
+        role: "Rdps",
+      },
+      {
+        name: "Marksmanship",
+        role: "Rdps",
+      },
+      {
+        name: "Survival",
+        role: "Mdps",
+      },
+    ],
+  },
+  {
+    class: "Mage",
+    specs: [
+      {
+        name: "Arcane",
+        role: "Rdps",
+      },
+      {
+        name: "Fire",
+        role: "Rdps",
+      },
+      {
+        name: "Frost",
+        role: "Rdps",
+      },
+    ],
+  },
+  {
+    class: "Paladin",
+    specs: [
+      {
+        name: "Holy",
+        role: "Healer",
+      },
+      {
+        name: "Protection",
+        role: "Tank",
+      },
+      {
+        name: "Retribution",
+        role: "Mdps",
+      },
+    ],
+  },
+  {
+    class: "Priest",
+    specs: [
+      {
+        name: "Discipline",
+        role: "Healer",
+      },
+      {
+        name: "Holy",
+        role: "Healer",
+      },
+      {
+        name: "Shadow",
+        role: "Rdps",
+      },
+    ],
+  },
+  {
+    class: "Rogue",
+    specs: [
+      {
+        name: "Assassination",
+        role: "Mdps",
+      },
+      {
+        name: "Combat",
+        role: "Mdps",
+      },
+      {
+        name: "Subtlety",
+        role: "Mdps",
+      },
+    ],
+  },
+  {
+    class: "Shaman",
+    specs: [
+      {
+        name: "Elemental",
+        role: "Rdps",
+      },
+      {
+        name: "Enhancement",
+        role: "Mdps",
+      },
+      {
+        name: "Restoration",
+        role: "Healer",
+      },
+    ],
+  },
+  {
+    class: "Warlock",
+    specs: [
+      {
+        name: "Affliction",
+        role: "Rdps",
+      },
+      {
+        name: "Demonology",
+        role: "Rdps",
+      },
+      {
+        name: "Destruction",
+        role: "Rdps",
+      },
+    ],
+  },
+  {
+    class: "Warrior",
+    specs: [
+      {
+        name: "Arms",
+        role: "Mdps",
+      },
+      {
+        name: "Fury",
+        role: "Mdps",
+      },
+      {
+        name: "Protection",
+        role: "Tank",
+      },
+    ],
+  },
+];
---
+++ packages/database/src/seedData/server.ts
@@ -0,0 +1,172 @@
+export const servers = [
+  {
+    server: "Gehennas",
+    region: "EU",
+  },
+  {
+    server: "Benediction",
+    region: "US",
+  },
+  {
+    server: "Faerlina",
+    region: "US",
+  },
+  {
+    server: "Пламегор",
+    region: "EU",
+  },
+  {
+    server: "Whitemane",
+    region: "US",
+  },
+  {
+    server: "Grobbulus",
+    region: "US",
+  },
+  {
+    server: "Golemagg",
+    region: "EU",
+  },
+  {
+    server: "Venoxis",
+    region: "EU",
+  },
+  {
+    server: "Pagle",
+    region: "US",
+  },
+  {
+    server: "Firemaw",
+    region: "EU",
+  },
+  {
+    server: "Mankrik",
+    region: "US",
+  },
+  {
+    server: "Arugal",
+    region: "US",
+  },
+  {
+    server: "Everlook",
+    region: "EU",
+  },
+  {
+    server: "Mograine",
+    region: "EU",
+  },
+  {
+    server: "Mirage Raceway",
+    region: "EU",
+  },
+  {
+    server: "Pyrewood Village",
+    region: "EU",
+  },
+  {
+    server: "Atiesh",
+    region: "US",
+  },
+  {
+    server: "Earthshaker",
+    region: "EU",
+  },
+  {
+    server: "Auberdine",
+    region: "EU",
+  },
+  {
+    server: "Sulfuron",
+    region: "EU",
+  },
+  {
+    server: "Eranikus",
+    region: "US",
+  },
+  {
+    server: "Lakeshire",
+    region: "EU",
+  },
+  {
+    server: "Razorfen",
+    region: "EU",
+  },
+  {
+    server: "Westfall",
+    region: "US",
+  },
+  {
+    server: "Ashkandi",
+    region: "US",
+  },
+  {
+    server: "Sulfuras",
+    region: "US",
+  },
+  {
+    server: "Nethergarde Keep",
+    region: "EU",
+  },
+  {
+    server: "Old Blanchy",
+    region: "US",
+  },
+  {
+    server: "Windseeker",
+    region: "US",
+  },
+  {
+    server: "Mandokir",
+    region: "EU",
+  },
+  {
+    server: "Thekal",
+    region: "EU",
+  },
+  {
+    server: "Remulos",
+    region: "US",
+  },
+  {
+    server: "Bloodsail Buccaneers",
+    region: "US",
+  },
+  {
+    server: "Skyfury",
+    region: "US",
+  },
+  {
+    server: "Хроми",
+    region: "EU",
+  },
+  {
+    server: "Yojamba",
+    region: "US",
+  },
+  {
+    server: "Hydraxian Waterlords",
+    region: "EU",
+  },
+  {
+    server: "Azuresong",
+    region: "US",
+  },
+  {
+    server: "Patchwerk",
+    region: "EU",
+  },
+  {
+    server: "Transcendence",
+    region: "EU",
+  },
+  {
+    server: "Jin'do",
+    region: "EU",
+  },
+  {
+    server: "Giantstalker",
+    region: "EU",
+  },
+];
+
+export const factions = ["Horde", "Alliance"];
