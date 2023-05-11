bfcf5d1e1d8a1261134962fc436eac5f0f5f6444
Alvadda <pansch.c@t-online.de>
2023-05-11 23:55:24 +0200

    nextauth url in inv

--- apps/web/src/utils/envs.ts
+++ apps/web/src/utils/envs.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 const envVariables = z.object({
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
+  NEXTAUTH_URL: z.string(),
 })
 
 export const envs = envVariables.parse(process.env)
